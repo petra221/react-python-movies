d47ccb47784b47294d81b302eaa954482336c9ec
Student <student@d17-327e-pc-03.ipa.ii.agh.edu.pl>
2025-01-26 10:29:00 +0100

    some 2nd sunday

--- ui/src/App.js
+++ ui/src/App.js
@@ -8,6 +8,8 @@ function App() {
     const [movies, setMovies] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
 
+    // const [deletingMovie, setDeletemovie] = usestate();  <<-- to jest zle. wywalic
+
     // function handleAddMovie(movie) {
     //     setMovies([...movies, movie]);
     //     setAddingMovie(false);
@@ -23,26 +25,41 @@ function App() {
       
 
         if (response.ok) {  //server odpowiada z responsem na ok, // moment przerwy w await mozna wstawic animacje czekania/ladowania
-          setMovies([...movies, movie]);
+          const movieFromServer = await response.json();      
+          setMovies([...movies, movieFromServer]);  /// odwolanie w tablicy 
           setAddingMovie(false);
         }
       }
+
+    async function handleDeleteMovie(movie) {
+      const response = await fetch(`/movies/${movie.id}`, {  // backtics //  /movies/movie_ID   - template strings, zmienna id moze miec url / movies/45 o id=45
+          method: 'DELETE',
+      });
+      if (response.ok) {
+          // const delmovies = onDeleteMovie();  <----- to jest zle!
+          // setMovies(delmovies);   <----- to jest zle!
+          setMovies(movies.filter(m => m !== movie));
+          
+      }
+  }
+///// sprawdz jak wyglada bez id - gdzie problem pojawia sie ''+{} ale {} +''  <-- w console
+
 
 // hook use effect <= udeEffect / read / zrob testy bez ponizszego hooka na f12 na chromie
 //najpierw powinien byc stan a pozniej hooki ale to dobre praktyki. najwazniejsze aby deklaracje i resszta byly przed return :)
 // przeczytaj o double-invoking ze strony zadania! dlaczego renderuje to 2 razy...
 
-      useEffect(() => {
-        const fetchMovies = async () => {
-            const response = await fetch(`/movies`);
-            if (response.ok) {
-                const movies = await response.json();
-                setMovies(movies);
-            }
-        };
-        fetchMovies(); // funkcja wywpołana od razu po deklaracji
-    }, []);  /// ta tablica - za kazdym razem jak zmieni sie identyfikator komponentu i tu bedzie stan komponentu wiec jesli stan sie zmieni to wtedy wyrenderuje. wiec pusta tablica jest po to aby sprawdzac zmiane stanu.. poczytaj i wspomoz sie chatemgpt
-    
+useEffect(() => {
+  const fetchMovies = async () => {
+      const response = await fetch(`/movies`);
+      if (response.ok) {
+          const movies = await response.json();
+          setMovies(movies);
+      }
+  };
+  fetchMovies(); // funkcja wywpołana od razu po deklaracji
+}, []);  /// ta tablica - za kazdym razem jak zmieni sie identyfikator komponentu i tu bedzie stan komponentu wiec jesli stan sie zmieni to wtedy wyrenderuje. wiec pusta tablica jest po to aby sprawdzac zmiane stanu.. poczytaj i wspomoz sie chatemgpt
+
 
     return (
         <div className="container">
@@ -50,13 +67,22 @@ function App() {
             {movies.length === 0
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
-                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                              onDeleteMovie={handleDeleteMovie} // tuu byl kod: (movie) => setMovies(movies.filter(m => m !== movie))wewnatrz {}
                 />}
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
                              buttonLabel="Add a movie"
                 />
                 : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
+{/* 
+            {deletingMovie
+                ? <MovieForm onMovie ={handleDeleteMovie}
+                             buttonLabel="Delete movie"
+                />
+                : <button onClick={() => setAddingMovie(true)}>Delete movie</button>} */}
+
+
+
         </div>
     );
 }
